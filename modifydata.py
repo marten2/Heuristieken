9009f07bca52647f67781d1d8665a27f66fe80bb
jenny9 <jenny.hasenack@gmail.com>
2015-04-08 15:35:45 +0200

    hoi

--- modifydata.py
+++ modifydata.py
@@ -1,5 +1,3 @@
-# gets longest list
-
 def getLongest(list):
 	''' Returns country with most connections '''
 	longest = []
@@ -8,10 +6,32 @@ def getLongest(list):
 			longest = element[1].length
 	return longest
 
-def makeColor(colorlist, country, color):
+def makeColor(countryColorList, country, color):
 	''' Colors a country '''
-	colorlist[country] == color
-	return countrylist
+	countryColorList[country] == color
+	return countryColorList
+
+# colors = [blue, red, yellow, green]
+def determineColor(colors, edgeData, country, countryColorList):
+	''' Returns appropriate color for country '''
+	temp = edgeData[country]
+	i = 0
+	for border in temp[1]:
+		if countryColorList[border] == colors[i]:
+			i++
+	print colors[i]
+	return colors[i]
+
+def selectCountry(edgeData, longest, countryColorList):
+''' Returns right country to be colored, using a breadth-first algorithm '''
+	
+	''''countryColorList[longest] == colors[0]
+	country = edgeData[longest]
+
+	for i in range (0, len(edgeData)):
+		for neighbours in country[1]:
+			return country'''
+
 
 
 
