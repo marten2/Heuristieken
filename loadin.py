b8d7cc84069785847d74fad3de375d26b1c74804
marten2 <marten.folkertsma@gmail.com>
2015-04-08 12:11:15 +0200

    loadin.py made

---
+++ loadin.py
@@ -0,0 +1,20 @@
+import csv
+
+def loadData(filename):
+	output = []
+	with open(filename, "r") as fin:
+		data = fin.readlines()[1:]
+		for i, d in enumerate(data):
+			land = d.split(";")[1:]
+			temp = []
+			print i
+			for j, a  in enumerate(land):
+				print a
+				if int(a) == 1:
+					temp.append(j)
+
+			output.append([i, temp])
+	return output
+
+if __name__ == "__main__":
+	loadData("IndiaData.csv")
