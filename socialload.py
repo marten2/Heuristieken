fb6eeb61ed417d54f1733b69388aa113fbabb2b7
marten2 <marten.folkertsma@gmail.com>
2015-04-15 12:04:22 +0200

    socialload.py debuged

--- socialload.py
+++ socialload.py
@@ -9,20 +9,25 @@ def loadData(filename):
 
 		length = getLength(data)
 		output = []
-		for i in range(length -1):
+		for i in range(length + 1):
 			output.append([i,[]])
 
+		print output, len(output)
+
 		for d in data:
+			print d[0], d[1]
 			output[d[0]][1].append(d[1])
 			output[d[1]][1].append(d[0])
 
-		print output 
+		return output
 
 def getLength(data):
 	high = 0 
-	for l in lines:
+	for l in data:
 		for a in l:
 			if a > high:
 				high = a
-
+				print high
 	return high
+if __name__ == "__main__":
+	loadData("network1.txt")
