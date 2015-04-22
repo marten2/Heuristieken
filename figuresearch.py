5aa40e217d393784a4367c3b6fafec2c7917dc0f
marten2 <marten.folkertsma@gmail.com>
2015-04-22 14:39:37 +0200

    random

--- figuresearch.py
+++ figuresearch.py
@@ -1,32 +1,28 @@
 # program that gets all connections figures, for maps these are tuples, triangles, sqaures
 def buildFigures(edgeData):
 	output = []
+	#print edgeData
 	# loop over all data trying to find connection figuures
 	for e in edgeData:
+		checklist= [e[1]]
+		temp = [e[0], []]
 
 		# checklist invelops all the figures of the triangle 
 
-		checklist = [e[0]]
-		# look per connection country 
-		for a in e[1]:
-			print checklist
-			temp = [e[0],[a]]
-			recursivebuild(temp, a, edgeData, checklist)
-			output.append(temp)
-
+		# look per connection country 		
 	return output
 
-# go through data seeing if a data point is connected to all other data points
-def recursivebuild(temp, a, edgeData, checklist):
-	
-	# loop through connected data points checking if data is connected to other data point in figure. 
-	if a not in checklist:
-		for b in edgeData[a][1]:
-			if b in checklist:
-				# add item if connected and check if there are more items in the connection
-				temp[1].append(b)
-				checklist.append(a)
-				print checklist
-				temp = recursivebuild(temp, b, edgeData, checklist)
+def recursivebuild(element, edgeData, temp, checklist):
+	for e in element[1]:
+		for l in edgeData[e][1]:
+			if l not in checklist:
+				break
+			else:
+				checklist.append(edgeData[e][1])
 
-	return temp
+				temp = recursivebuild(edgeData[l])
+
+
+
+if __name__ == "__main__":
+	print buildFigures([[0,[1,2,3]],[1,[0,2,4]],[2,[0,1,4]],[3,[0,4]],[4,[1,2,3]]])
