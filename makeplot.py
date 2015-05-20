0b796310adb653bcf55b1896aa5bd29b0884db8d
marten2 <marten.folkertsma@gmail.com>
2015-05-20 18:10:40 +0200

    line fix

--- makeplot.py
+++ makeplot.py
@@ -10,21 +10,17 @@ def makePlot(filename):
 	#plt.scatter(data2['a'], data2['b'], c='r', alpha=0.3)
 	m, b = np.polyfit(data['x'], data['y'], 1)
 	print m, b
-	x = np.arrange(0, 100, 0.1)
-	y = np.(m*x + b)
-	plt.plot(x, y)
+	x = np.array([0, 110])
+	print x
+	y = (m*x + b)
+	print y
+	plt.plot(x, y, "r-")
 	plt.scatter(data['x'], data['y'], c='b', alpha=0.1)
-	m,b = polyfit(data['x'], data['y'], 1)
+	plt.axis([0, 110, 1, 10])
 	plt.xlabel("Biggest clique")
 	plt.ylabel("Total amount of colors used")
 	plt.show()
 
-<<<<<<< HEAD
-
 if __name__ == "__main__":
 
 	makePlot('Marten_hillclimber_connectiviteit.csv')
-
-=======
-makePlot('jenny_degree_connections.csv')
->>>>>>> fecba0fa9e2037beef1591ce675cfb0ad10e044a
