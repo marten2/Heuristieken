47376b98551ce83ed066e5be93b8248b86773daf
jenny9 <jenny.hasenack@gmail.com>
2015-05-24 14:45:07 +0200

    ff conflict vookromen

--- makeplot.py
+++ makeplot.py
@@ -8,17 +8,17 @@ def makePlot(filename):
 	#data2 = np.genfromtxt('tot_con_vs_col_10000_cycles_1000_nodes_1000_10000_con.csv', delimiter=",", names=['a', 'b'])
 
 	#plt.scatter(data2['a'], data2['b'], c='r', alpha=0.3)
-	m, b = np.polyfit(data['x'], data['y'], 1)
-	x = np.array([0, 110])
-	y = (m*x + b)
-	plt.plot(x, y, "r-")
+	#m, b = np.polyfit(data['x'], data['y'], 1)
+	#x = np.array([0, 20])
+	#y = (m*x + b)
+	#plt.plot(x, y, "r-")
 	plt.scatter(data['x'], data['y'], c='b', alpha=0.1)
-	plt.axis([0, 110, 1, 10])
+	#plt.axis([0, 10, 0, 10])
 
-	plt.xlabel("Biggest clique")
+	plt.xlabel("Maximum degree")
 	plt.ylabel("Total amount of colors used")
 	plt.show()
 
 if __name__ == "__main__":
 
-	makePlot('Marten_hillclimber_connectiviteit.csv')
+	makePlot('Marten_Hybrid_degree.csv')
